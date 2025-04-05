824713aff9688170aa2a8cb50b2d110f90d1f449
Nikita Zhiga <nvzhiga@avito.ru>
2025-04-06 00:35:54 +0300

    fix dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "minesweeper-html5/",
+  base: "/minesweeper-html5/",
   plugins: [react()],
 })

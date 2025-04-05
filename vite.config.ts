d10b1bed633debd856f3b4da67d2d110e99f3329
Nikita Zhiga <nvzhiga@avito.ru>
2025-04-05 23:34:45 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://assisken.github.io/minesweeper-html5/",
   plugins: [
     vue(),
     vueDevTools(),

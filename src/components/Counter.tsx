c30b10738fb2be9cd20bf1cbf0b7aa56dca1faa2
Nikita Zhiga <nvzhiga@avito.ru>
2025-04-22 02:30:03 +0300

    wip segment background timer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,61 +2,72 @@ import { Image } from 'react-konva'
 import { useImage } from 'react-konva-utils'
 
 import background from '../assets/display/nums_background.svg'
-// import display0 from '../assets/display/d0.svg'
-// import display1 from '../assets/display/d1.svg'
-// import display2 from '../assets/display/d2.svg'
-// import display3 from '../assets/display/d3.svg'
-// import display4 from '../assets/display/d4.svg'
-// import display5 from '../assets/display/d5.svg'
-// import display6 from '../assets/display/d6.svg'
-// import display7 from '../assets/display/d7.svg'
-// import display8 from '../assets/display/d8.svg'
-// import display9 from '../assets/display/d9.svg'
-import React from 'react'
+import React, { useState } from 'react'
+import { Segment } from './Segment'
 
 export function Counter(props: { x: number; y: number; height: number; alightRight?: boolean }) {
     const backgroundProps = { width: 173, height: 108 }
     const backgroundWidth = (props.height / backgroundProps.height) * backgroundProps.width
 
-    // const segmentProps = { width: 52, height: 99 }
-    // const segmentWidth = (props.height / segmentProps.height) * segmentProps.width
+    function segmentProps(n: number) {
+        const segmentPadding = 20
+        const offset = 34
+
+        return {
+            x: props.x - (props.alightRight ? backgroundWidth : 0) + n * segmentPadding - offset,
+            y: props.y + 3,
+            width: 52,
+            height: props.height - 6,
+        }
+    }
+    const firstSegment = segmentProps(1)
+    const secondSegment = segmentProps(2)
+    const thirdSegment = segmentProps(3)
 
     const [backgroundTexture, backgroundTextureLoaded] = useImage(background)
 
-    // const [display0Texture, display0TextureLoaded] = useImage(display0)
-    // const [display1Texture, display1TextureLoaded] = useImage(display1)
-    // const [display2Texture, display2TextureLoaded] = useImage(display2)
-    // const [display3Texture, display3TextureLoaded] = useImage(display3)
-    // const [display4Texture, display4TextureLoaded] = useImage(display4)
-    // const [display5Texture, display5TextureLoaded] = useImage(display5)
-    // const [display6Texture, display6TextureLoaded] = useImage(display6)
-    // const [display7Texture, display7TextureLoaded] = useImage(display7)
-    // const [display8Texture, display8TextureLoaded] = useImage(display8)
-    // const [display9Texture, display9TextureLoaded] = useImage(display9)
+    let [time, setTime] = useState(0)
 
-    React.useEffect(() => {}, [
-        [
-            backgroundTextureLoaded,
-            // display0TextureLoaded,
-            // display1TextureLoaded,
-            // display2TextureLoaded,
-            // display3TextureLoaded,
-            // display4TextureLoaded,
-            // display5TextureLoaded,
-            // display6TextureLoaded,
-            // display7TextureLoaded,
-            // display8TextureLoaded,
-            // display9TextureLoaded,
-        ].every((v) => v == 'loaded'),
-    ])
+    async function timer() {
+        while (true) {
+            await new Promise(() => {
+                console.log(time)
+                setTimeout(() => setTime(time + 1), 1000)
+            })
+        }
+    }
+
+    React.useEffect(() => {
+        timer()
+    }, [[backgroundTextureLoaded].every((v) => v == 'loaded')])
 
     return (
-        <Image
-            x={props.x - (props.alightRight ? backgroundWidth : 0)}
-            y={props.y}
-            width={backgroundWidth}
-            height={props.height}
-            image={backgroundTexture}
-        />
+        <>
+            <Image
+                x={props.x - (props.alightRight ? backgroundWidth : 0)}
+                y={props.y}
+                width={backgroundWidth}
+                height={props.height}
+                image={backgroundTexture}
+            />
+            <Segment
+                x={firstSegment.x}
+                y={firstSegment.y}
+                height={firstSegment.height}
+                displayNumber={Math.floor(time / 100) % 10}
+            />
+            <Segment
+                x={secondSegment.x}
+                y={secondSegment.y}
+                height={secondSegment.height}
+                displayNumber={Math.floor(time / 10) % 10}
+            />
+            <Segment
+                x={thirdSegment.x}
+                y={thirdSegment.y}
+                height={thirdSegment.height}
+                displayNumber={time % 10}
+            />
+        </>
     )
 }
---
+++ src/components/Segment.tsx
@@ -0,0 +1,75 @@
+import { useImage } from 'react-konva-utils'
+import { Image } from 'react-konva'
+import { useState } from 'react'
+import React from 'react'
+
+import display0 from '../assets/display/d0.svg'
+import display1 from '../assets/display/d1.svg'
+import display2 from '../assets/display/d2.svg'
+import display3 from '../assets/display/d3.svg'
+import display4 from '../assets/display/d4.svg'
+import display5 from '../assets/display/d5.svg'
+import display6 from '../assets/display/d6.svg'
+import display7 from '../assets/display/d7.svg'
+import display8 from '../assets/display/d8.svg'
+import display9 from '../assets/display/d9.svg'
+
+export function Segment(props: { displayNumber: number; x: number; y: number; height: number }) {
+    const segmentProps = { width: 52, height: 99 }
+    const actualWidth = (props.height / props.height) * segmentProps.width
+
+    const [display0Texture, display0TextureLoaded] = useImage(display0)
+    const [display1Texture, display1TextureLoaded] = useImage(display1)
+    const [display2Texture, display2TextureLoaded] = useImage(display2)
+    const [display3Texture, display3TextureLoaded] = useImage(display3)
+    const [display4Texture, display4TextureLoaded] = useImage(display4)
+    const [display5Texture, display5TextureLoaded] = useImage(display5)
+    const [display6Texture, display6TextureLoaded] = useImage(display6)
+    const [display7Texture, display7TextureLoaded] = useImage(display7)
+    const [display8Texture, display8TextureLoaded] = useImage(display8)
+    const [display9Texture, display9TextureLoaded] = useImage(display9)
+
+    React.useEffect(() => {
+        setTexture(textureByNumber.get(props.displayNumber))
+    }, [
+        [
+            display0TextureLoaded,
+            display1TextureLoaded,
+            display2TextureLoaded,
+            display3TextureLoaded,
+            display4TextureLoaded,
+            display5TextureLoaded,
+            display6TextureLoaded,
+            display7TextureLoaded,
+            display8TextureLoaded,
+            display9TextureLoaded,
+        ].every((v) => v == 'loaded'),
+    ])
+
+    const textureByNumber = new Map([
+        [0, display0Texture],
+        [1, display1Texture],
+        [2, display2Texture],
+        [3, display3Texture],
+        [4, display4Texture],
+        [5, display5Texture],
+        [6, display6Texture],
+        [7, display7Texture],
+        [8, display8Texture],
+        [9, display9Texture],
+    ])
+
+    const [texture, setTexture] = useState(display0Texture)
+
+    return (
+        <>
+            <Image
+                x={props.x}
+                y={props.y}
+                width={actualWidth}
+                height={props.height}
+                image={texture}
+            />
+        </>
+    )
+}
